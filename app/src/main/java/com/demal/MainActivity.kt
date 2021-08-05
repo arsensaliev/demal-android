a69152d4051a91776830f1e0be464b21187c8713
Vladimir Svishch <5693031@gmail.com>
2021-08-05 15:35:54 +0300

    Пустой проект

---
+++ app/src/main/java/com/demal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
