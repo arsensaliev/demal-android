e7745a3b196e64b2eb09a2f9f1ce66b341d5da0e
Romeo <ua.roma2005@gmail.com>
2021-08-10 14:32:08 +0300

    refactor: changed package name in AndroidManifest.xml

--- feature-profile/src/main/java/com/demal/feature_profile/view/ProfileFragment.kt
+++ feature-profile/src/main/java/com/demal/feature_profile/view/ProfileFragment.kt
@@ -1,7 +1,7 @@
 package com.demal.feature_profile.view
 
 import androidx.fragment.app.Fragment
-import com.demal.`feat-profile`.R
+import com.demal.feature_profile.R
 
 //Только для теста навигации
 class ProfileFragment : Fragment(R.layout.fragment_profile)
