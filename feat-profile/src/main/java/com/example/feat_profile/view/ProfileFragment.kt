0ac6542d6595e3f1f7b1de1a6b82d2839191e9c4
Romeo <ua.roma2005@gmail.com>
2021-08-10 11:55:09 +0300

    refactor: changed some directions

--- feat-profile/src/main/java/com/example/feat_profile/view/ProfileFragment.kt
+++ feat-profile/src/main/java/com/example/feat_profile/view/ProfileFragment.kt
@@ -1,7 +1,7 @@
 package com.example.feat_profile.view
 
 import androidx.fragment.app.Fragment
-import com.example.feat_profile.R
+import com.demal.`feat-profile`.R
 
 //Только для теста навигации
 class ProfileFragment : Fragment(R.layout.fragment_profile)
