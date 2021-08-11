3594422c50d77d36558d91242f305433cd7536ef
Romeo <ua.roma2005@gmail.com>
2021-08-11 11:03:20 +0300

    refactor: refactored app module

--- app/src/main/java/com/demal/app/App.kt
+++ app/src/main/java/com/demal/app/App.kt
@@ -3,7 +3,9 @@ package com.demal.app
 import androidx.multidex.MultiDexApplication
 import com.demal.koin.modules.ciceroneModule
 import com.demal.koin.modules.navigatorsModule
+import com.demal.koin.modules.retrofitModule
 import com.demal.koin.modules.viewModelModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : MultiDexApplication() {
@@ -11,11 +13,14 @@ class App : MultiDexApplication() {
         super.onCreate()
 
         startKoin {
+            androidContext(this@App)
+
             modules(
                 listOf(
                     viewModelModule,
                     ciceroneModule,
-                    navigatorsModule
+                    navigatorsModule,
+                    retrofitModule
                 )
             )
         }
--- app/src/main/java/com/demal/koin/modules/KoinModules.kt
+++ app/src/main/java/com/demal/koin/modules/KoinModules.kt
@@ -5,9 +5,16 @@ import com.demal.navigation.MainActivityNavigator
 import com.demal.navigation.Navigator
 import com.demal.navigation.Screens
 import com.demal.feature_profile.navigation.ProfileNavigator
+import com.demal.repository.RetrofitImplementation
+import com.demal.repository.api.BaseInterceptor
 import com.github.terrakok.cicerone.Cicerone
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val ciceroneModule = module {
     val cicerone = Cicerone.create()
@@ -28,4 +35,20 @@ val navigatorsModule = module {
 
 val viewModelModule = module {
     viewModel { MainViewModel(get()) }
+}
+
+val retrofitModule = module {
+    single {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(BaseInterceptor.interceptor)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(RetrofitImplementation.BASE_URL_LOCATIONS)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(client)
+            .build()
+    }
 }
