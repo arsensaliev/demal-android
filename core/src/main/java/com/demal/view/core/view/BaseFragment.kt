e29e65f877ef1583450c250272aa468d1a7a89b7
Romeo2005 <ua.roma2005@gmail.com>
2021-08-15 12:08:34 +0300

    refactor: moved livedata observing to onViewCreated

--- core/src/main/java/com/demal/view/core/view/BaseFragment.kt
+++ core/src/main/java/com/demal/view/core/view/BaseFragment.kt
@@ -1,6 +1,7 @@
 package com.demal.view.core.view
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
@@ -24,9 +25,10 @@ abstract class BaseFragment<VB : ViewBinding, D : AppStateEntity, VM : BaseViewM
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel.stateLiveData.observe(this) { state ->
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.stateLiveData.observe(viewLifecycleOwner) { state ->
             renderData(state)
         }
     }
