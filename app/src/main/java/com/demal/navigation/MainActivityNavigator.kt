265ad0345c6af08b9e96e4db7a498b7c09905ef8
Romeo2005 <ua.roma2005@gmail.com>
2021-08-18 14:24:24 +0300

    refactor refactored navigators extension hierarchy

--- app/src/main/java/com/demal/navigation/MainActivityNavigator.kt
+++ app/src/main/java/com/demal/navigation/MainActivityNavigator.kt
@@ -2,4 +2,4 @@ package com.demal.navigation
 
 import com.demal.view.core.BaseNavigator
 
-interface MainActivityNavigator : MainBottomNavigation, BaseNavigator
+interface MainActivityNavigator : BaseNavigator, MainBottomNavigation
