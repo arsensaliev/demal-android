193c90ae1ff1949b52f059076d21ef558f97efdc
NikKonshin <konbshin1996@gmail.c0m>
2021-08-20 14:44:31 +0300

    refactor: corrected comments Roman

--- feature-mytours/src/main/java/com/demal/view/MyToursFragment.kt
+++ feature-mytours/src/main/java/com/demal/view/MyToursFragment.kt
@@ -1,19 +1,6 @@
 package com.demal.view
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.demal.feature_mytours.R
 
-class MyToursFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_my_tours, container, false)
-    }
-
-}
+class MyToursFragment : Fragment(R.layout.fragment_my_tours) {}
