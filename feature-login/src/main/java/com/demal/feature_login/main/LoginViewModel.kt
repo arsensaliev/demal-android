11c275b4e378a1f349cf7fad188265aea1f8ff79
app-droid <alinadorozhkina91@mail.ru>
2021-08-27 14:45:57 +0300

    feat: add userRepository to LoginViewModel

--- feature-login/src/main/java/com/demal/feature_login/main/LoginViewModel.kt
+++ feature-login/src/main/java/com/demal/feature_login/main/LoginViewModel.kt
@@ -2,6 +2,10 @@ package com.demal.feature_login.main
 
 import com.demal.feature_login.navigation.LoginNavigator
 import com.demal.model.data.entity.login.LoginStatus
+import com.demal.repository.repository.UserRepository
 import com.demal.view.core.view_model.BaseViewModel
 
-class LoginViewModel(navigator: LoginNavigator) : BaseViewModel<LoginStatus>(navigator)
+class LoginViewModel(
+    navigator: LoginNavigator,
+    private val repository: UserRepository
+) : BaseViewModel<LoginStatus>(navigator)
