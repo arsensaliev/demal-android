55af57a6c716d8aa71d12842d15775d0bc57a690
DTag <Dentag@mail.ru>
2021-08-30 15:48:41 +0300

    feature: koin dependencies added

--- app/src/main/java/com/demal/koin/modules/ViewModelModules.kt
+++ app/src/main/java/com/demal/koin/modules/ViewModelModules.kt
@@ -1,6 +1,7 @@
 package com.demal.koin.modules
 
 import com.demal.feature_profile.main.ProfileViewModel
+import com.demal.feature_wishlist.main.WishlistViewModel
 import com.demal.main.MainViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { MainViewModel(get()) }
     viewModel { ProfileViewModel(get(), get()) }
+    viewModel { WishlistViewModel(get(), get(), get()) }
 }
