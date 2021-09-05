a1c1d6e2c16cf3d61cd284b6450f9718698f6c55
NikKonshin <konbshin1996@gmail.c0m>
2021-09-05 21:54:13 +0300

    feature: implemented tour filtering

---
+++ feature-mytours/src/main/java/com/demal/model/DateIntegerFormat.kt
@@ -0,0 +1,10 @@
+package com.demal.model
+
+data class DateIntegerFormat(
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val hour: Int,
+    val minutes: Int,
+    val seconds: Int
+)
