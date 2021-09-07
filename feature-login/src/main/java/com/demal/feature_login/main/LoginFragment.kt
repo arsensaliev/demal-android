9afeefe5b20edb253db11f81a4c12fb9186406de
app-droid <alinadorozhkina91@mail.ru>
2021-09-07 16:42:02 +0300

    feat: add textWatcher, navigation to Profile

--- feature-login/src/main/java/com/demal/feature_login/main/LoginFragment.kt
+++ feature-login/src/main/java/com/demal/feature_login/main/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.demal.feature_login.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,8 +19,8 @@ class LoginFragment :
 
     override var bindingNullable: FragmentLoginBinding? = null
     override val viewModel: LoginViewModel by viewModel()
-
-    private val validator = AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT)
+    private val emailValidator = AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT)
+    private val passwordValidator = AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,19 +33,30 @@ class LoginFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initValidation()
+        setupListeners()
+        initEmailValidation()
+        initPasswordValidation()
         checkValidation()
     }
 
-    private fun initValidation() = with(binding) {
-        validator.addValidation(
-            textInputLayoutEmail,
+    private fun setupListeners() = with(binding) {
+        editTextPassword.addTextChangedListener(TextFieldValidationTextWatcher(editTextPassword))
+        editTextEmail.addTextChangedListener(TextFieldValidationTextWatcher(editTextEmail))
+    }
+
+    private fun initEmailValidation() {
+        emailValidator.setTextInputLayoutErrorTextAppearance(R.style.ErrorTextAppearance)
+        emailValidator.addValidation(
+            binding.textInputLayoutEmail,
             android.util.Patterns.EMAIL_ADDRESS,
             getString(R.string.wrong_email)
         )
+    }
 
-        validator.addValidation(
-            textInputLayoutPassword,
+    private fun initPasswordValidation() {
+        passwordValidator.setTextInputLayoutErrorTextAppearance(R.style.ErrorTextAppearance)
+        passwordValidator.addValidation(
+            binding.textInputLayoutPassword,
             getString(R.string.regex_password_validation),
             getString(R.string.password_rule)
         )
@@ -51,20 +64,45 @@ class LoginFragment :
 
     private fun checkValidation() = with(binding) {
         buttonLogin.setOnClickListener {
-            if (validator.validate()) {
-                // успешно
+            if (emailValidator.validate() && passwordValidator.validate()) {
+                viewModel.login(
+                    binding.editTextEmail.text.toString().trim(),
+                    binding.editTextPassword.text.toString().trim()
+                )
             } else {
                 renderMessage(getString(R.string.input_error))
             }
         }
     }
 
+    override fun renderError(error: Throwable) {
+        renderMessage(getString(R.string.no_auth))
+    }
+
     override fun renderSuccess(data: LoginStatus) {
-        TODO("Not yet implemented")
+        viewModel.navigator.toProfileScreen()
     }
 
     companion object {
         fun newInstance() = LoginFragment()
+    }
+
+    inner class TextFieldValidationTextWatcher(private val view: View) : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // not used
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            // not used
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            when (view.id) {
+                R.id.edit_text_email -> emailValidator.validate()
+
+                R.id.edit_text_password -> passwordValidator.validate()
+            }
+        }
     }
 
 }
