1c8f443845378c8200eb0e69105d643912010b63
app-droid <alinadorozhkina91@mail.ru>
2021-09-07 16:44:29 +0300

    feat: add login checking

--- feature-login/src/main/java/com/demal/feature_login/main/LoginViewModel.kt
+++ feature-login/src/main/java/com/demal/feature_login/main/LoginViewModel.kt
@@ -1,11 +1,33 @@
 package com.demal.feature_login.main
 
+import android.annotation.SuppressLint
 import com.demal.feature_login.navigation.LoginNavigator
+import com.demal.model.data.app_state.BaseState
 import com.demal.model.data.entity.login.LoginStatus
+import com.demal.model.data.exceptions.NoAuthException
 import com.demal.repository.repository.UserRepository
 import com.demal.view.core.view_model.BaseViewModel
 
 class LoginViewModel(
-    navigator: LoginNavigator,
+    val navigator: LoginNavigator,
     private val repository: UserRepository
-) : BaseViewModel<LoginStatus>(navigator)
+) : BaseViewModel<LoginStatus>(navigator){
+
+    private var status = LoginStatus(true)
+
+    fun login(
+        email: String,
+        password: String
+    ) = runAsync {
+        repository.login(email, password)
+        mStateLiveData.postValue(BaseState.Success(status))
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun handleError(error: Throwable) {
+        if (error is NoAuthException) {
+            mStateLiveData.postValue(BaseState.Error(error))
+        }
+    }
+    
+}
