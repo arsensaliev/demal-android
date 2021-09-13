feaaaa364be6dfbdc76694ee46819c633702b26e
Koshake <koshake1@mail.com>
2021-09-13 13:54:24 +0300

    fix: Category

---
+++ model/src/main/java/com/demal/model/data/entity/category/network/CategoriesResponse.kt
@@ -0,0 +1,5 @@
+package com.demal.model.data.entity.category.network
+
+import com.demal.model.data.entity.tours.network.CategoryResponse
+
+data class CategoriesResponse(val categories: List<CategoryResponse>)
